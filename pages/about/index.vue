fc9749798ea0eb55d06d15b46066a5fd2d14603a
Michal Krištof <mkristof@bykd.com>
2023-01-16 11:07:59 +0100

    fix route to images

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -22,7 +22,7 @@
       </p>
     </div>
     <div class="flex flex-col items-center">
-      <img src="images/portrait.jpg" alt="portrait" class="ml-3 w-9/12 h-auto rotate-6 rounded-xl">
+      <img src="~/public/images/portrait.jpg" alt="portrait" class="ml-3 w-9/12 h-auto rotate-6 rounded-xl">
       <ul class="flex flex-col w-9/12 mt-10 font-medium text-sm gap-2">
         <li class="flex flex-row group gap-3 items-center hover:text-teal-400 duration-300 cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/></svg>
